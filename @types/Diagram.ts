a5f63e3b6372e6b70a8333d0af22e6790723c18b
feddot2517@gmail.com <8D2g1101>
2020-11-24 19:56:22 +0600

    add onSelectNode props, dist build

--- @types/Diagram.ts
+++ @types/Diagram.ts
@@ -4,7 +4,7 @@ import { DiagramSchema } from './DiagramSchema';
 export type DiagramProps<P> = {
   schema?: DiagramSchema<P>;
   onChange?: (schema: DiagramSchema<P>) => unknown;
-  onEvent?: (e: {event: string, value: any})=>unknown
+  onSelectNode?: (e: {event: string, value: any})=>unknown
 };
 
 declare const Diagram: <P = unknown>(props: DiagramProps<P>) => JSX.Element;
